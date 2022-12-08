f6bd5b9d414990525e43c0deb79851bb78d7d3dc
sysopsgeo <oskogorev@outlook.com>
2022-12-08 13:56:22 +0400

    initial commit

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+    region = "us-east-1"
+    access_key =  "AKIAUQDIWPF7ULM43ADQ" 
+    secret_key = "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"
+}
+
+variable "subnet_cidr_block" {
+  description =  "subnet cidr block"
+
+}
+
+variable "vpc_cidr_block" {
+  description = "cidr block for vpc"
+}
+
+resource "aws_vpc" "development-vpc" {
+  cidr_block = var.vpc_cidr_block
+  tags = {
+    Name: "development"
+  }
+}
+
+resource "aws_subnet" "dev-subnet-1" {
+    vpc_id = aws_vpc.development-vpc.id
+    cidr_block = var.subnet_cidr_block
+    availability_zone = "us-east-1b"
+    tags = {
+        Name: "development-sub-1b"
+    }
+}
+
